fd23ab9cd9c4e86a59865c8f91da7d50d160552c
Sarfaraz Alam <sarfaraz7277alam@gmail.com>
2025-09-11 19:05:15 +0530

    basic setup commplete

--- server/server.js
+++ server/server.js
@@ -1,10 +1,33 @@
 const express = require('express')
-const PORT = 5050
+const morgan = require('morgan')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const colors = require('colors')
+
+// donfig dotenv
+dotenv.config();
+
+// rest object
 const app = express()
 
+
+// middlewares
+app.use(morgan('dev'))
+app.use(cors())
+app.use(express.json())
+
+
+
+// routes
 app.get('/' , (req , res )=>{
-    res.send('Hello World')
+    res.send('<h1>Hello form express</h1>')
 })
+
+
+//port
+const PORT = process.env.PORT || 4040
+
+// listenig to the server
 app.listen(PORT , ()=>{
-    console.log(`Server is running on port ${PORT}`)
+    console.log(`http://localhost:${PORT}`.bgCyan.white);
 })
