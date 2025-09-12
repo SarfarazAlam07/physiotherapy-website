060a0c09c9ff1ea41335f6c358a49329624912ff
Sarfaraz Alam <sarfaraz7277alam@gmail.com>
2025-09-13 01:01:42 +0530

    setupFrontend

---
+++ client/src/components/Layouts/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className=''>
+        <h6 className='text-3xl font-bold underline'>All rights reserved &copy; mirani</h6>
+    </div>
+  )
+}
+
+export default Footer
