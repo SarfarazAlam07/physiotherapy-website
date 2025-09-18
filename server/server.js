004ba00e34bc3384f166db3360c66da92d1a5fa4
Sarfaraz Alam <sarfaraz7277alam@gmail.com>
2025-09-18 22:58:51 +0530

    test

--- server/server.js
+++ server/server.js
@@ -1,1 +1,21 @@
-ÿþ
+const express  = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const app = express();
+const PORT = process.env.PORT || 4040
+
+// Middleware
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req , res )=>{
+    res.send('Hello World!')
+})
+
+
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
